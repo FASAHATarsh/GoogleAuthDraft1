5659627fce0277e28da22126353811be02e2021f
FASAHATarsh <68802584+FASAHATarsh@users.noreply.github.com>
2021-08-29 09:00:18 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+import firebase from 'firebase'
+export const firebaseConfig = {
+    apiKey: "AIzaSyBuR7fCZCSdUVZV6Z-YapmltO5aOkFODWU",
+    authDomain: "storytelling-app-4a838.firebaseapp.com",
+    projectId: "storytelling-app-4a838",
+    storageBucket: "storytelling-app-4a838.appspot.com",
+    messagingSenderId: "780932734003",
+    appId: "1:780932734003:web:4d47a84dd687108d80d746"
+};
